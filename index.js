bde162d0e5c2ff43a796c1db6b10d43befe838d3
Natheem Yousuf <natheem@natheemy.com>
2019-02-04 13:10:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var api = new ParseServer({
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || '123456',
   masterKey: process.env.MASTER_KEY || '123456', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://bingochat.herokuapp.com/parse',  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'https://bingochat.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
